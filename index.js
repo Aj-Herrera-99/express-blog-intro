ea572004cb56c0649d612aafc9b1cda7c8063398
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-02 13:36:01 +0100

    apertura server

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/**
+    Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo.
+    - Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+    - Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+    - Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array.
+    - Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+    - Testare su postman
+ */
 const express = require("express");
 const app = express();
-console.log("Hello World");
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Server del mio blog</h1>`);
+})
+
+app.listen(PORT, () => {
+    console.log("Server aperto");
+})
