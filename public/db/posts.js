633ce12b7c2684937f76d1886ffea9354e3b5484
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-02 14:00:55 +0100

    creazione fake db in cui ci ho messo un array di posts

---
+++ public/db/posts.js
@@ -0,0 +1,34 @@
+const dolci = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Descrizione del ciambellone",
+        immagine: "images/ciambellone.jpeg",
+        tags: ["tag1", "tag2"],
+    },
+    {
+        titolo: "Cracker di barbabietola",
+        contenuto: "Descrizione dei cracker di barbabietola",
+        immagine: "images/cracker_barbabietola.jpeg",
+        tags: ["tag3", "tag4"],
+    },
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "Descrizione del pane fritto dolce",
+        immagine: "images/pane_fritto_dolce.jpeg",
+        tags: ["tag5", "tag6"],
+    },
+    {
+        titolo: "Pasta di barbabietola",
+        contenuto: "Descrizione della pasta di barbabietola",
+        immagine: "images/pasta_barbabietola.jpeg",
+        tags: ["tag7", "tag8"],
+    },
+    {
+        titolo: "Torta paesana",
+        contenuto: "Descrizione della torta paesana",
+        immagine: "images/torta_paesana.jpeg",
+        tags: ["tag9", "tag10"],
+    },
+];
+
+module.exports = {dolci}
